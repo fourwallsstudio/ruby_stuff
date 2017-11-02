fcbbc0cd7599b6be94d0c8d287e99dc6f76ba1b9
Calvin McElroy <fourwallsstudio@gmail.com>
2017-11-02 18:54:23 -0400

    add insertion sort

---
+++ sorts/insertionsort.rb
@@ -0,0 +1,27 @@
+# O(n**2)
+def instertionsort(arr)
+  return arr if arr.length <= 1
+
+  i = 1
+  while i < arr.length
+
+    j = i - 1
+    while j >= 0
+      # compairing could be reduced to O(log n) by using
+      # binary search to find index. Swapping would still be O(n) if
+      # inserting into beginning/mid of array. Swapping wouldn't be
+      # an issue if sorting a linked list.
+      break if arr[j] <= arr[j+1]
+      arr[j], arr[j+1] = arr[j+1], arr[j]
+      j -= 1
+    end
+
+    i += 1
+  end
+
+  arr
+end
+
+arr = [5, 4, 3, 2, 6, 7, 3, 4, 1]
+
+puts instertionsort(arr)

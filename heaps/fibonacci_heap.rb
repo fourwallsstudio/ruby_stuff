aeffb78efb3784d39fc11086af0e5d43ae81de8f
Calvin McElroy <fourwallsstudio@gmail.com>
2017-11-08 23:03:00 -0500

    add fibonacci heap

---
+++ heaps/fibonacci_heap.rb
@@ -0,0 +1,21 @@
+class Node
+  def initialize
+    @parent = nil
+    @key = nil
+    @degree = nil
+    @marked = false
+    @child = nil
+    @left = nil
+    @right = nil
+  end
+end
+
+
+
+class FibonacciHeap
+
+  def initialize
+    @min = nil
+  end
+
+end
